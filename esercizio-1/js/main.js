1594663a0f85940dc417e6ea664bdb63f4e341ac
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-03-21 16:59:18 +0100

    boolterm css ex primo

--- esercizio-1/js/main.js
+++ esercizio-1/js/main.js
@@ -1,7 +1,13 @@
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 
 // inizializzo le variabili
-var firstNum, secNum, msg;
+var firstNum, secNum, msg, d, hours, minutes;
+
+
+
+d = new Date();
+hours = d.getHours();
+minutes = d.getMinutes();
 
 // chiedo due numeri in successione
 firstNum = prompt('Primo numero?')
@@ -9,13 +15,15 @@ secNum = prompt('Secondo numero?')
 
 // stabilisco qual è il numero maggiore
 if (firstNum > secNum) {
-  msg = 'Il numero maggiore è: ' + firstNum;
+  msg = 'Il numero maggiore è ' + "<span style=\"color:white\">" + firstNum + "</span>";
 } else if (firstNum < secNum) {
-  msg = 'Il numero maggiore è: ' + secNum;
+  msg = 'Il numero maggiore è ' + "<span style=\"color:white\">" + secNum + "</span>";
 } else {
   msg = 'I numeri sono uguali'
 }
 
 // stampo a video il numero maggiore
 
+document.getElementById('user').innerHTML = '[agrossano@me]';
 document.getElementById('numero').innerHTML = msg;
+document.getElementById('time').innerHTML = hours + ":" + minutes;
